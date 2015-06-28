750366137a5a8407f83f1dc82f9021513c086940
Jeison Giraldo <jeisongiraldo16@gmail.com>
2015-06-28 12:14:03 -0500

    Estructura de la cabecera

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
 	<?php include 'inc/header_common.php'; ?>
 </head>
 <body>
-	<h1>Workshop Desarrollo web con Bootstrap 3</h1>
-
-
+	<?php include 'inc/header.php';?>
 	<?php include 'inc/footer_common.php'; ?>
 </body>
 </html>
