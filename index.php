f62b0bfacd30a0ba37280e93e5b6526c6a066138
Jeison Giraldo <jeisongiraldo16@gmail.com>
2015-06-28 11:18:47 -0500

    Estructura de carpetas y reutilizacion de codigo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Workshop con Bootstrap 3</title>
+	<?php include 'inc/header_common.php'; ?>
+</head>
+<body>
+	<h1>Workshop Desarrollo web con Bootstrap 3</h1>
+
+
+	<?php include 'inc/footer_common.php'; ?>
+</body>
+</html>
